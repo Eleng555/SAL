1588f36350da4cc4008a97bbea39e9bc5f2b7819
Eleng555 <ice_el@sbcglobal.net>
2014-05-12 21:51:51 -0500

    tested some basic game functionality

---
+++ NetBeansProjects/Super Adventure Land Test/src/pkgsuper/adventure/land/test/Ball.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * Tutorial: https://www.youtube.com/watch?v=OPz1X9aQURc&list=PL87AA2306844063D2
+ */
+
+package pkgsuper.adventure.land.test;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Emily Leng
+ */
+public class Ball {
+    private int x = 400;
+    private int y = 25;
+    private int radius = 20;
+    private int dx = 0;
+    private int dy = 0;
+    private double gravity = 16;
+    private double energyLoss = 1;
+    private double changeInTime = .2;
+    private double xFriction = .95;
+
+    public Ball() {
+    }
+    
+    public Ball(int i, int j) {
+        x = i;
+        y = j;
+    }
+    
+    public void moveRight(){
+        if (dx+1 < 20)
+        dx += 1;
+    }
+    
+    public void moveLeft(){
+        if (dx-1 > -20)
+            dx -= 1;
+    }
+    
+    public void update(BallTest t){
+        if (x + dx > t.getWidth() - radius - 1){
+                x = t.getWidth() - radius - 1;
+                dx = -dx;}
+            else if (x + dx < 0 + radius){
+                x = 0 + radius;
+                dx = -dx;
+            }
+            else{
+                x += dx;
+            }
+            
+            //friction
+            if (y == t.getHeight() - radius - 1){//bottom of applet
+                dx *= xFriction;
+                if (Math.abs(dx) < .8){
+                    dx = 0;
+                }
+            }
+            
+            if (y > t.getHeight() - radius - 1){
+                y = t.getHeight() - radius - 1;
+                dy *= energyLoss;
+                dy = -dy;
+            }
+            else {
+                dy += gravity * changeInTime; //velocity formula
+                y += dy * changeInTime + .5*gravity*changeInTime*changeInTime; //physics equation for position
+            
+            }
+            
+    }
+    
+    public void paint(Graphics g){
+        g.setColor(Color.CYAN);
+        g.fillOval(x-radius,y-radius,radius*2,radius*2);
+           
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
+    public double getEnergyLoss() {
+        return energyLoss;
+    }
+
+    public void setEnergyLoss(double energyLoss) {
+        this.energyLoss = energyLoss;
+    }
+
+    public double getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(double gravity) {
+        this.gravity = gravity;
+    }
+    
+    
+    }
+   
+
---
+++ NetBeansProjects/Super Adventure Land Test/src/pkgsuper/adventure/land/test/BallTest.java
@@ -0,0 +1,113 @@
+/* * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkgsuper.adventure.land.test;
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+/**
+ *
+ * @author Emily Leng
+ */
+public class BallTest extends Applet implements Runnable, KeyListener{
+    Ball b,b2;
+    Platform p;
+    private Image i;
+    private Graphics doubleBuffer;
+    
+    @Override
+    //initializes applet, called first
+    public void init() {
+        setSize(800, 600); //sets window size
+        addKeyListener(this);
+    }
+
+    //called after init method
+    public void start() {
+        b = new Ball();
+        b2 = new Ball(250,250);
+        p = new Platform();
+        Thread thread1 = new Thread(this); //this refers to the run method defined below
+        thread1.start();
+    }
+    
+    //used in the start method for the thread
+    @Override
+    public void run() {
+        //thread information
+        while(true){ //restrict x and y to be within window size, bonces off walls
+            b.update(this);
+            b2.update(this);
+            p.update(this);
+            repaint();
+            try{ //if it can't sleep print an exception
+            Thread.sleep(17); //repaints about 64 frames per second, sleep for 17 milliseconds
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void stop() {
+        
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+
+    @Override
+    public void update(Graphics g) { //double buffering, prevents flickering when painting
+        if(i == null){
+            i = createImage(this.getSize().width, this.getSize().height);
+            doubleBuffer = i.getGraphics();
+        }
+        doubleBuffer.setColor(getBackground());
+        doubleBuffer.fillRect(0,0,this.getSize().width,this.getSize().height);
+        doubleBuffer.setColor(getForeground());
+        paint(doubleBuffer);
+        g.drawImage(i, 0, 0, this);
+    }
+
+    
+    
+    @Override
+    public void paint(Graphics g) { //prints cyan circle
+        b.paint(g);
+        b2.paint(g);
+        p.paint(g);
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e){
+        switch(e.getKeyCode()){ //KeyCode returns int of key pressed
+                case KeyEvent.VK_LEFT: //left arrow key
+                    b.moveLeft();
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    b.moveRight();
+                    break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+    
+    
+    
+    }
