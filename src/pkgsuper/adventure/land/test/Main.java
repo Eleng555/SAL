61386e9e67f803521990cc699f8db990868209c7
mmurali6 <mmurali6@10.215.58.33>
2014-05-15 15:08:54 -0500

    Hello1

---
+++ src/pkgsuper/adventure/land/test/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkgsuper.adventure.land.test;
+
+/**
+ *
+ * @author mmurali6
+ */
+public abstract class Main extends Character {
+    public Main()
+    {
+        
+    }
+}
