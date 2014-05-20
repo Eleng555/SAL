ca2970c4973756d3a55239f211fcad8b7e668c4c
TheForgottenMyth <ashvikaas@gmail.com>
2014-05-20 15:22:00 -0500

    Basic shell of Quest class

---
+++ NetBeansProjects/Super Adventure Land Test/src/pkgsuper/adventure/land/test/Quest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkgsuper.adventure.land.test;
+
+/**
+ *
+ * @author vrangan6
+ */
+public class Quest {
+    String description;
+    int xCoordinate;
+    int yCoordinate;
+    
+    
+    public Quest(String s, int x, int y){
+    description=s;
+    yCoordinate=y;
+    xCoordinate=x;
+    
+    }
+    
+    public void paint(){
+        
+    }
+    }
+    
+    
+    
+    
+
