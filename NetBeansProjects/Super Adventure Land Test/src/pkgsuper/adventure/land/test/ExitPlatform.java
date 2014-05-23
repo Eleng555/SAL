d3afad2e71e077053ce65206d064deadc5b6761e
Eleng555 <ice_el@sbcglobal.net>
2014-05-23 14:48:15 -0500

    Create ExitPlatform.java

---
+++ NetBeansProjects/Super Adventure Land Test/src/pkgsuper/adventure/land/test/ExitPlatform.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkgsuper.adventure.land.test;
+
+/**
+ *
+ * @author Emily Leng
+ */
+public class ExitPlatform extends Platform{
+
+    public ExitPlatform(int x, int y) {
+        super(x,y);
+    }
+    
+    
+}
