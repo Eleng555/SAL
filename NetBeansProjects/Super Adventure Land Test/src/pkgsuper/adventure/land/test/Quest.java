263c2bc8fb83b0f823f6633aea1dce055c247d68
Eleng555 <ice_el@sbcglobal.net>
2014-05-27 14:40:23 -0500

    Update Quest.java

--- NetBeansProjects/Super Adventure Land Test/src/pkgsuper/adventure/land/test/Quest.java
+++ NetBeansProjects/Super Adventure Land Test/src/pkgsuper/adventure/land/test/Quest.java
@@ -9,6 +9,9 @@ package pkgsuper.adventure.land.test;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Graphics;
+import java.awt.TextField;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
@@ -17,22 +20,26 @@ import java.awt.event.MouseMotionListener;
  * @author vrangan6
  */
 public class Quest implements MouseMotionListener{
-    String description;
-    int xCoordinate;
-    int yCoordinate;
-    boolean completed;
+    private String description;
+    private int xCoordinate;
+    private int yCoordinate;
+    private TextField input = new TextField(50);
+    private String [] answers = {"jamesgosling", "19", "thematch", "stopimagining", "oak", "210"};
+    
     
     public Quest(String s, int x, int y){
     description=s;
     yCoordinate=y;
     xCoordinate=x;
-    completed=false;
+    
     }
     
+  
     
     public void paint(Graphics g){
         g.setColor(Color.WHITE);
         g.drawString(description, xCoordinate, yCoordinate);
+        
     }
     
     public void remove(Graphics g){
@@ -43,7 +50,7 @@ public class Quest implements MouseMotionListener{
         int x = evt.getX();
         int y = evt.getY();
 
-        if (x>=) {
+        if (evt.getClickCount() >= 2) {
           
         }
     }
@@ -64,5 +71,12 @@ public class Quest implements MouseMotionListener{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    public boolean isCorrect(String s){
+        for (String answer : answers){
+            if (answer.equals(s))
+                return true;
+        }
+        return false;
+    }
     }
     
