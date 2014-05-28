c40b40c755283e48e6705a4d69ed7e4bf26245e5
Eleng555 <ice_el@sbcglobal.net>
2014-05-27 23:30:39 -0500

    added documentation

--- NetBeansProjects/Super Adventure Land Test/src/pkgsuper/adventure/land/test/QuestBox.java
+++ NetBeansProjects/Super Adventure Land Test/src/pkgsuper/adventure/land/test/QuestBox.java
@@ -8,21 +8,19 @@ package pkgsuper.adventure.land.test;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Emily Leng
  */
 public class QuestBox extends Platform{
-    
-    private int dx;
     private int x, y, width, height;
     private Character ch;
-    private boolean collide = false, passable = true;
+    private boolean collide = false;
     private int id;
     private boolean activation;
     public QuestBox() {
-    dx = -5; 
     x = 300;
     y = 300;
     width = 75;
@@ -31,25 +29,27 @@ public class QuestBox extends Platform{
     activation=false;
     }
     
+    /*
+    Creates a Quest box with height, width, index, and x and y coordinates.
+    */
     public QuestBox(int x, int y, Character c,int i){
         this.x = x;
         this.y = y;
         width = 75;
         height = 110;
-        dx = -5;
         ch = c;
         id=i;
         activation=false;
     }
     
+    /*
+    Paints the QuestBox and allows user input for answers.
+    */
     public void paint(Graphics g){
         g.setColor(Color.GREEN);
         g.fillRect(x,y,width,height);
         if(activation==true){
-            for(Quest q:LevelOne.getQuests()){
-                if(q.getId()==this.getId())
-                    q.paint(g);
-            }
+            
         }
             
     }
@@ -67,29 +67,35 @@ public class QuestBox extends Platform{
     public int getId(){
         return id;
     }
-    
+    /*
+    Checks if the Character collides with the QuestBox, and reverses its direction so it bounces off it.
+    */
     public boolean checkForCollision(Character c){
         int ballX = c.getX();
         int ballY = c.getY();
         int radius = c.getRadius();
-        if (passable){
         //checks if ball is within platform, if yes, reposition and reverse direction
         if (ballX + radius  >= x && ballX <= x + width/2 && ballY >= y && ballY + radius + radius <= y + height ){
             double newDx = c.getDx();
-            if (c.getDx () < 0)
-            c.setX(x - radius - 1);
+            //if (c.getDx () < 0)
+            //c.setX(x - radius - 1);
             c.setDx((int)-newDx);
             collide = true;
     }
         if (ballX + radius  >= x + width/2 && ballX <= x + width && ballY >= y && ballY + radius + radius <= y + height ){
             double newDx = c.getDx();
-            c.setX(x + radius + width + 1);
+            //c.setX(x + radius + width + 1);
             c.setDx((int)-newDx);
             collide = true;
-    }
 }
             
     
         return collide;
     }
+
+    public int getWidth() {
+        return width;
+    }
+    
+    
 }
